dc82eb2f4f82bffba2485744481a1e95460886a9
Henrique <hfvmarques@gmail.com>
2022-02-22 13:26:38 -0400

    feat(welcome_index): implemented dashboard page implemented dashboard page with admin statistics of quantity on each model used in app

--- app/controllers/admins_backoffice/welcome_controller.rb
+++ app/controllers/admins_backoffice/welcome_controller.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class AdminsBackoffice::WelcomeController < AdminsBackofficeController
-  def index; end
+  def index
+    @subjects = Subject.all.count
+    @authors = Author.all.count
+    @publishers = Publisher.all.count
+    @books = Book.all.count
+    @loans = Loan.all.count
+    @book_types = BookType.all.count
+  end
 end
