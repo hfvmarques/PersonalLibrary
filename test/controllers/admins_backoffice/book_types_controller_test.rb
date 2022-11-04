a64e1c70620a31685e6e27a647de9b688f48377d
Henrique Marques <hfvmarques@gmail.com>
2022-11-03 20:16:02 -0400

    housekeeping: removes mini test files since we're using only rspec

--- test/controllers/admins_backoffice/book_types_controller_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class AdminsBackoffice::BookTypesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/site/search_controller_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class Site::SearchControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
